d6f7761035ca942a4d684d680719e03e7af76b02
kodastrategic <kodastrategic.bot@proton.me>
2026-07-21 17:05:55 -0300

    fix: inclui supabase-config.js para deploy Vercel

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://erfnaigmgtrmuxlnpfje.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyZm5haWdtZ3RybXV4bG5wZmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NTYyMzgsImV4cCI6MjEwMDIzMjIzOH0.-WelInevPIF9FocS2EhyeVj_YMbz3mE8jTHdtccv5xI'
+};
